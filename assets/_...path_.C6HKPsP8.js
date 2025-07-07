316187f61d5c5da9b22b0dc063eeb215166be060
mete0rxsc <Mete0r_xsc@hotmail.com>
2025-07-07 18:07:07 +0800

    2025/07/07-1

---
+++ assets/_...path_.C6HKPsP8.js
@@ -0,0 +1,1 @@
+import{a,u as s,e as t}from"./chunks/vue-router.CFm7we44.js";import{f as n,H as r,P as c,O as p}from"./framework.B3-meWAy.js";const u=n({__name:"[...path]",setup(f){const o=s(),e=a();return r(()=>{e.path!=="/"&&e.path.endsWith("/")&&o.replace(e.path.slice(0,-1))}),(i,m)=>(p(),c("div"))}});typeof t=="function"&&t(u);export{u as default};
---
+++ assets/back.Dsijg7lu.js
@@ -0,0 +1,1 @@
+import{u as o}from"./chunks/vue-router.CFm7we44.js";function t(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{t as u};
